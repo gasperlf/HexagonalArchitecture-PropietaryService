f7a8d2b441a2510a878a53ed7d60c6d4cc592cab
Angie <angiep2008@hotmail.com>
2023-03-08 13:16:39 -0500

    fix: userResponse

--- powerup-user/src/main/java/com/powerup/user/application/dto/UserResponse.java
+++ powerup-user/src/main/java/com/powerup/user/application/dto/UserResponse.java
@@ -9,9 +9,6 @@ import lombok.Setter;
 public class UserResponse {
     private Long id;
     private String name;
-    private String lastName;
-    private String phone;
     private String email;
-    private Role role;
-    private String idDocument;
+
 }
